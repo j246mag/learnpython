a78c654e58075cd316ba31bd041ac4fb65a74728
Johannes <magnusson.johannes@gmail.com>
2016-03-02 19:41:13 +0100

    ex6

---
+++ ex6.py
@@ -0,0 +1,32 @@
+# Variable
+x = "There are %d types of people." % 10
+# Variable
+binary = "binary"
+#Variable
+do_not = "don't"
+# Variable
+y = "Those who know %s and those who %s." %(binary, do_not)
+
+# Print variable x
+print x
+# Print variable y
+print y
+
+# repeats variable x
+print "I said: %r." % x
+# repeats variable y
+print "I also said: '%s'." % y
+
+# Set varible
+hilarious = False
+# Set string inside string
+joke_evaluation = "Isn't that joke so funny?! %r"
+
+# set string inside string
+print joke_evaluation % hilarious
+
+w = "This is the left side of..."
+e = "a string with a right side."
+
+print w + e
+
